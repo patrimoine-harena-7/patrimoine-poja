3720a91bef76ca7318b2469e13af233f7c5510b3
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2024-08-01 17:42:12 +0300

    refactor(JacksonConfig)

--- src/main/java/com/haren/api/config/JacksonConfig.java
+++ src/main/java/com/haren/api/config/JacksonConfig.java
@@ -1,26 +1,23 @@
 package com.haren.api.config;
 
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.haren.api.serializer.LocalDateSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
 
+@Configuration
 public class JacksonConfig {
 
-    public static ObjectMapper createObjectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        SimpleModule module = new SimpleModule();
-        module.addSerializer(LocalDate.class, new LocalDateSerializer());
-        mapper.registerModule(module);
-        return mapper;
-    }
-
-    public static void main(String[] args) throws Exception {
-        ObjectMapper mapper = createObjectMapper();
-        LocalDate date = LocalDate.now();
-        String json = mapper.writeValueAsString(date);
-        System.out.println(json);
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .registerModule(new Jdk8Module());
     }
 }
 
