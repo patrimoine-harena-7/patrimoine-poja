40e3144aa4ccebb2919b817fa978e95920419efb
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2024-08-06 10:45:37 +0300

    refactor: ObjectMapperTest & JacksonConfig

--- src/test/java/com/haren/api/unit/objectMapper/ObjectMapperTest.java
+++ src/test/java/com/haren/api/unit/objectMapper/ObjectMapperTest.java
@@ -7,29 +7,35 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
 import com.haren.api.conf.FacadeIT;
 import java.time.Instant;
+
+import com.haren.api.config.JacksonConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@SpringBootTest
+@ContextConfiguration(classes = {JacksonConfig.class})
 public class ObjectMapperTest extends FacadeIT {
   @Autowired ObjectMapper injectedBean;
   ObjectMapper newInstance = new ObjectMapper();
 
   SomeClassWithDatetimeField someClassWithDatetimeField =
-      new SomeClassWithDatetimeField(Instant.now());
+          new SomeClassWithDatetimeField(Instant.now());
 
   @Test
   void new_instance_throws_on_java_datetime_module() {
     String jsonString = someClassWithDatetimeField.toJsonString();
     assertThrows(
-        InvalidDefinitionException.class,
-        () -> newInstance.readValue(jsonString, SomeClassWithDatetimeField.class));
+            InvalidDefinitionException.class,
+            () -> newInstance.readValue(jsonString, SomeClassWithDatetimeField.class));
   }
 
   @Test
   void injected_bean_handles_java_datetime_module() {
     assertDoesNotThrow(
-        () ->
-            injectedBean.readValue(
-                someClassWithDatetimeField.toJsonString(), SomeClassWithDatetimeField.class));
+            () ->
+                    injectedBean.readValue(
+                            someClassWithDatetimeField.toJsonString(), SomeClassWithDatetimeField.class));
   }
 }
